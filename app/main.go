f643be377614940bb65a1fed425bb7a0418596ee
Bhava Sagar <bhavasagar09@gmail.com>
2025-03-02 23:33:38 +0530

    update support for backslash inside of quotes

--- app/main.go
+++ app/main.go
@@ -131,10 +131,10 @@ func extractArgsAndCmd(input_str string) (string, []string) {
 
 		if char == rune('"') && !open_single_quote && !prev_backslash {
 			open_double_quote = !open_double_quote
-		} else if char == rune('\\') && !open_double_quote && !open_single_quote && !prev_backslash {
-			prev_backslash = true
 		} else if char == rune('\'') && !open_double_quote && !prev_backslash {
 			open_single_quote = !open_single_quote
+		} else if char == rune('\\') && !prev_backslash {
+			prev_backslash = true
 		} else if char == rune(' ') && !prev_backslash {
 			if open_single_quote || open_double_quote {
 				curr += string(char)
